022c08a5fb5cc5c2a83137a9c5a888cc435ae16f
francescoantici <francesco.antici98@gmail.com>
2024-04-15 11:45:06 +0900

    Update files

--- models/containers.py
+++ models/containers.py
@@ -2,7 +2,7 @@ from dependency_injector import containers, providers
 import os
 
 from service.job_characterizer.fugaku_job_characterizer import FugakuJobCharacterizer
-from service.data_fetcher.parquet_data_fetcher import ParquetDataFetcher
+from service.data_fetcher.fugaku_data_fetcher import FugakuDataFetcher
 from service.classification_model.knn import KNN
 from service.feature_encoder.sb_feature_encoder import SBEncoder
 
@@ -13,7 +13,7 @@ class Container(containers.DeclarativeContainer):
     config = providers.Configuration(json_files=["config/config.json"])
     
     # Data fetcher 
-    data_fetcher = providers.Singleton(ParquetDataFetcher, config.data_file) 
+    data_fetcher = providers.Singleton(FugakuDataFetcher) 
     
     # Prediction model for the jobs
     classification_model = providers.Singleton(KNN.from_saved_model, config.model_weights_path) 
